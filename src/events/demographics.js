fd3c2e233c6c1cd6015f437e4c1c3c25da3717a8
fire-finch <firefinchdev@gmail.com>
2018-08-15 23:40:23 +0530

    Add: src files

---
+++ src/events/demographics.js
@@ -0,0 +1,14 @@
+const { models } = require("../db/models");
+
+function eventDemgraphicCreated (demoId, userId) {
+
+}
+
+function eventDemgraphicUpdated (demoId, userId) {
+
+}
+
+module.exports = {
+  eventDemgraphicCreated,
+  eventDemgraphicUpdated
+}
---
+++ src/events/users.js
@@ -0,0 +1,14 @@
+const { models } = require("../db/models");
+
+function eventUserCreated (userId) {
+
+}
+
+function eventUserUpdated (userId) {
+
+}
+
+module.exports = {
+  eventUserCreated,
+  eventUserUpdated
+}
